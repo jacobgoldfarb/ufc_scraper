417203db8202e8a0c452ec8d188c3cff6be52810
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2022-07-19 22:48:11 -0400

    Added singular insert method for a single division

--- persistence/postgres_db.py
+++ persistence/postgres_db.py
@@ -15,11 +15,18 @@ class PostgresDatabase(Database):
     
     def add_fighter_from_rankings(self, rankings: list[Ranking]):
         sql_values = sum([self._marshall_rankings_for_db(ranking) for ranking in rankings], [])
-
+        return self._insert_fighter(sql_values)
+    
+    # Singular to concurrently "interweave" page parsing and db update in the future.
+    def add_fighter_from_ranking(self, ranking: Ranking):
+        sql_values = self._marshall_rankings_for_db(ranking)
+        return self._insert_fighter(sql_values)
+    
+    def _insert_fighter(self, values):
         sql_query = """INSERT INTO fighters (full_name, division, ranking) VALUES %s 
         ON CONFLICT (division, ranking) DO NOTHING;"""
         psycopg2.extras.execute_values (
-            self.cursor, sql_query, sql_values, template=None, page_size=100
+            self.cursor, sql_query, values, template=None, page_size=100
         )
         return self.connection.commit()
 
